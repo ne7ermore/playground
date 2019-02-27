fc9387ecb6630a8a096967fb8a37a36fbfbbf5f8
Tao <422618856@qq.com>
2019-02-27 17:56:06 +0800

    u

--- python/heap/skyline.py
+++ python/heap/skyline.py
@@ -1,32 +1,25 @@
-import heapq
+class Solution:
+    def getSkyline(self, buildings):
+        que, live = [0], []
+        skyline = []
+        pre = 0
+        for l, r, h in buildings:
+            live.append((l, -h))
+            live.append((r, h))
 
+        live.sort(key=lambda x: (x[0], x[1]))
 
-def get_skyline(lrh):
-    que, live = [0], []
-    skyline = []
-    for l, r, h in lrh:
-        heapq.heappush(live, (l, -h))
-        heapq.heappush(live, (r, h))
-
-    while live:
-        res = heapq.heappop(live)
-        if res[1] < 0:
-            if res[1] < que[0]:
-                skyline.append([res[0], -res[1]])
-
-            heapq.heappush(que, res[1])
-
-        else:
-            if -res[1] == que[0]:
-                heapq.heappop(que)
-                skyline.append([res[0], -que[0]])
+        while live:
+            res = live.pop(0)
+            if res[1] < 0:
+                que.append(-res[1])
+                que.sort()
             else:
-                que.remove(-res[1])
+                que.pop(que.index(res[1]))
 
-    return skyline
+            cur = que[-1]
+            if cur != pre:
+                skyline.append([res[0], cur])
+                pre = cur
 
-
-if __name__ == "__main__":
-    lrh = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
-    assert get_skyline(lrh) == [[2, 10], [3, 15], [7, 12], [
-        12, 0], [15, 10], [20, 8], [24, 0]]
+        return skyline
