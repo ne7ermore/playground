495d48c35496e45be2f0bf1e38c3281b13e5c173
Tao <422618856@qq.com>
2019-03-01 16:11:14 +0800

    u

---
+++ cpp/heap/kth-largest-element.cpp
@@ -0,0 +1,51 @@
+/**
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream. For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
+
+Example:
+
+int k = 3;
+int[] arr = [4,5,8,2];
+KthLargest kthLargest = new KthLargest(3, arr);
+kthLargest.add(3);   // returns 4
+kthLargest.add(5);   // returns 5
+kthLargest.add(10);  // returns 5
+kthLargest.add(9);   // returns 8
+kthLargest.add(4);   // returns 8
+Note: 
+You may assume that nums' length ≥ k-1 and k ≥ 1.
+ **/
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class KthLargest
+{
+    priority_queue<int, vector<int>, greater<int>> q;
+    int size;
+
+  public:
+    KthLargest(int k, vector<int> nums)
+    {
+        size = k;
+        for (auto &a : nums)
+        {
+            q.push(a);
+            if (q.size() > k)
+            {
+                q.pop();
+            }
+        }
+    }
+
+    int add(int val)
+    {
+        q.push(val);
+        if (q.size() > size)
+            q.pop();
+        return q.top();
+    }
+};
