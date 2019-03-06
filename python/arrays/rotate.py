4885241363a0352a7eff8f96bfd92cde270a5bc5
Tao <422618856@qq.com>
2019-03-06 23:55:46 +0800

    u

--- python/arrays/rotate.py
+++
@@ -1,8 +0,0 @@
-def rotate(arr, k):
-    len_arr = len(arr)
-    k %= len_arr
-    return arr[len_arr - k:] + arr[:-k]
-
-
-if __name__ == "__main__":
-    assert rotate([1, 2, 3, 4, 5, 6, 7, 8], 4) == [5, 6, 7, 8, 1, 2, 3, 4]
