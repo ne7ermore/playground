dc5f3fa47d8853ae0713cbf78f14ce99f4d2e15b
Tao <422618856@qq.com>
2019-03-10 11:05:06 +0800

    u

---
+++ cpp/array/first-missing-positive.cpp
@@ -0,0 +1,48 @@
+/**
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+ * **/
+
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        nums.insert(nums.begin(), 0);
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] < 0 || nums[i] >= n)
+                nums[i] = 0;
+        }
+
+        for (int i = 1; i < n; i++)
+            nums[nums[i] % n] += n;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] / n == 0)
+                return i;
+        }
+        return n;
+    }
+};
