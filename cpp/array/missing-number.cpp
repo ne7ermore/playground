a019216603a04bd42f6aec075cc836f027b73b85
Tao <422618856@qq.com>
2019-03-11 14:05:00 +0800

    u

---
+++ cpp/array/missing-number.cpp
@@ -0,0 +1,30 @@
+/**
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+ * **/
+
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int missingNumber(vector<int> &nums)
+    {
+        int s = 0;
+        for (auto n : nums)
+            s += n;
+        return (nums.size() + 1) * nums.size() / 2 - s;
+    }
+};
