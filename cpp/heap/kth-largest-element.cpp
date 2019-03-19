c1dfe22933c6333e06f6b4d2ecf7531b2d21ab75
Tao <422618856@qq.com>
2019-03-19 08:41:30 +0800

    u

--- cpp/heap/kth-largest-element.cpp
+++ cpp/heap/kth-largest-element.cpp
@@ -1,7 +1,10 @@
 /**
-Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, 
+not the kth distinct element.
 
-Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream. For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
+Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, 
+which contains initial elements from the stream. For each call to the method KthLargest.add, 
+return the element representing the kth largest element in the stream.
 
 Example:
 
