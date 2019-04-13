ba50c03162910c429bc4a2080c655bd32b7cb90c
Tao <422618856@qq.com>
2019-04-13 20:24:02 +0800

    add bktree python

---
+++ python/tree/bktree.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+class BkTree(object):
+    def __init__(self, distance_fn):
+        self.dfn = distance_fn
+        self.root = None
+
+    def add(self, item):
+        node = self.root
+        if node is None:
+            node = (item, {})
+
+        while True:
+            p, children = node
+            d = self.dfn(item, p)
+            if d in children:
+                node = children[d]
+            else:
+                children[d] = (item, {})
+                break
+
+    def search(self, item, n):
+        if self.root is None:
+            return []
+
+        q = deque([self.root])
+        res = []
+        while q:
+            p, children = q.pop()
+            d = self.dfn(item, p)
+            if d <= n:
+                res.append((p, d))
+
+            for dist, c in children.items():
+                if d-n <= dist <= d+n:
+                    q.append(c)
+
+        res.sort(key=lambda x: x[1])
+        return res
