3c7350ac3df5251d3afe45c2d18c253924795305
Tao <422618856@qq.com>
2020-01-22 19:41:54 +0800

    Numbers At Most N Given Digit Set

---
+++ python/arrays/n-given-digit-set.py
@@ -0,0 +1,49 @@
+"""
+We have a sorted set of digits D, a non-empty subset of {'1','2','3','4','5','6','7','8','9'}.  (Note that '0' is not included.)
+
+Now, we write numbers using these digits, using each digit as many times as we want.  For example, if D = {'1','3','5'}, we may write numbers such as '13', '551', '1351315'.
+
+Return the number of positive integers that can be written (using the digits of D) that are less than or equal to N.
+
+ 
+
+Example 1:
+
+Input: D = ["1","3","5","7"], N = 100
+Output: 20
+Explanation: 
+The 20 numbers that can be written are:
+1, 3, 5, 7, 11, 13, 15, 17, 31, 33, 35, 37, 51, 53, 55, 57, 71, 73, 75, 77.
+Example 2:
+
+Input: D = ["1","4","9"], N = 1000000000
+Output: 29523
+Explanation: 
+We can write 3 one digit numbers, 9 two digit numbers, 27 three digit numbers,
+81 four digit numbers, 243 five digit numbers, 729 six digit numbers,
+2187 seven digit numbers, 6561 eight digit numbers, and 19683 nine digit numbers.
+In total, this is 29523 integers that can be written using the digits of D.
+ 
+
+Note:
+
+D is a subset of digits '1'-'9' in sorted order.
+1 <= N <= 10^9
+Accepted
+"""
+
+
+class Solution:
+    def atMostNGivenDigitSet(self, D: List[str], N: int) -> int:
+        N = str(N)
+        len_n = len(N)
+        out = sum(len(D) ** i for i in range(1, len_n))
+
+        i = 0
+        while i < len_n:
+            out += sum(N[i] > d for d in D) * (len(D)**(len_n-1-i))
+            if N[i] not in D:
+                break
+            i += 1
+
+        return out + (i == len_n)
