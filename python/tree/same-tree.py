7340d778b6cba883ed834e41c19dc71dcb6abac1
Tao <422618856@qq.com>
2020-01-27 18:01:33 +0800

    Same Tree

---
+++ python/tree/same-tree.py
@@ -0,0 +1,56 @@
+"""
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+"""
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        def dfs(q, p):
+            if (q and p) and (q.val == p.val):
+                return dfs(q.left, p.left) and dfs(q.right, p.right)
+
+            if not q and not p:
+                return True
+
+            return False
+
+        return dfs(p, q)
